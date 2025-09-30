6ae644edbdeaf4f5a80f51f1f1f2c0a472e160fa
Believer2001 <tsehbenoit2001@gmail.com>
2025-09-30 11:04:53 +0100

    first commit

---
+++ src/main/java/enset/ma/even_driven_kafka_spring/EvenDrivenKafkaSpringApplication.java
@@ -0,0 +1,13 @@
+package enset.ma.even_driven_kafka_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenDrivenKafkaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvenDrivenKafkaSpringApplication.class, args);
+    }
+
+}
